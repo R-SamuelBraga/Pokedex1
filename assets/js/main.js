49bb3dc2ea2caf1727e556c8abf2f5d4d3d2cd7b
Renan Braga <renansamuel.braga.ti@gmail.com>
2023-08-25 20:46:29 -0300

    requisições multiplas em paralelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,16 @@
-const offset = 0
-const limit = 100
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
-function convertPokemonToHml(pokemon){
+function convertPokemonToHtml(pokemon){
     return `
     <li class="pokemon">
-        <span class="number">pokemon</span>
+        <span class="number">#${pokemon.id}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                <li class="type">${pokemon.types}</li>
+                <li class="type">${pokemon.types}</li>
             </ol>
             <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg"
             alt="${pokemon.name}">
         </div>
     </li>`        
@@ -21,17 +18,17 @@ function convertPokemonToHml(pokemon){
 
 const  pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let index = 0; index < pokemons.length; index++) {
-            const pokemon = pokemons[index];
-            console.log(convertPokemonToHml(pokemon))
-            pokemonList.innerHTML += convertPokemonToHml(pokemon)
+pokeapi.getPokemons().then((pokemons = []) => {
+    // const listitems = []
+    // for (let index = 0; index < pokemons.length; index++) {
+        
+    //     
 
-        }
-    })
-    .catch((error) => console.error(error))
+    //     const pokemon = pokemons[index];
+    //     console.log(pokemon)
+    //     listitems.push(convertPokemonToHml(pokemon))
+    //     // pokemonList.innerHTML += 
 
-
+    // }
+    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+})
